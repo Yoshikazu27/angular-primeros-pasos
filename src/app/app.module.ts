e0d1decb89df5a8c601edc493523cedc6b2539c1
Yoshikazu Casanova <yoshikazu.casanova@gmail.com>
2024-09-19 19:13:13 -0500

    DBZ app creado. Counter corregido.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 
-import { CounterModule } from './counter/components/counter.module';
+import { CounterModule } from './counter/counter.module';
 
 import { AppComponent } from './app.component';
+import { DbzModule } from './dbz/dbz.module';
 import { HeroesModule } from './heroes/heroes.module';
 
 @NgModule({
@@ -15,6 +16,7 @@ import { HeroesModule } from './heroes/heroes.module';
     BrowserModule,
     AppRoutingModule,
     CounterModule,
+    DbzModule,
     HeroesModule
   ],
   providers: [],
