5f43a4d053c6ffc71a9dcaeecd31d1b075296a35
Yoshikazu Casanova <yoshikazu.casanova@gmail.com>
2024-09-19 19:43:17 -0500

    Agregados: dbz-add-character component y dbz-list component

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { AddCharacterComponent } from './components/add-character/add-character.component';
 import { MainPageComponent } from './pages/main-page.component';
+import { ListComponent } from './components/list/list.component';
 
 @NgModule({
   declarations: [
+    AddCharacterComponent,
+    ListComponent,
     MainPageComponent
   ],
   exports: [
+    AddCharacterComponent,
+    ListComponent,
     MainPageComponent
   ],
   imports: [
